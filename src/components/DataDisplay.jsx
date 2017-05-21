2273c7884682766b771bd6bc51d9cb871e37009b
Chan <chanthemancan@gmail.com>
2017-05-20 18:25:36 -0700

    Renders list of all uers with columns email, uid, name, and url. user_schools.passes down as undefined for unknown reason.

--- src/components/DataDisplay.jsx
+++ src/components/DataDisplay.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import DataDisplayItem from './DataDisplayItem.jsx';
 
 //props is the "show" object of whichever is selected
 
 class DataDisplay extends Component {
   constructor(props) {
     super(props);
-    console.log('DataDisplay has', this.props.data)
+    console.log('DataDisplay has', this.props.data);
   }
 
   render () {
@@ -14,8 +15,21 @@ class DataDisplay extends Component {
           <div> loading up the data! </div>
         )
     }
+    const dataItems = this.props.data.map((user) => {
+      return <DataDisplayItem key={user.user_id} data={user} />
+    });
     return (
-        <div> making progress </div>
+        <table>
+          <tbody>
+            <tr>
+              <th>email</th>
+              <th>id</th>
+              <th>name</th>
+              <th>url</th>
+            </tr>
+            {dataItems}
+          </tbody>
+        </table>
       );
   }
 }
--- src/components/DataDisplayItem.jsx
+++ src/components/DataDisplayItem.jsx
@@ -3,13 +3,17 @@ import React, {Component} from 'react';
 class DataDisplayItem extends Component {
   constructor (props) {
     super(props);
+    console.log('datadisplayitem has', this.props.data)
   }
 
   render () {
     return (
-        <div>
-          Data Display Item biiitchhhh
-        </div>
+        <tr>
+          <td>{this.props.data.user_email}</td>
+          <td>{this.props.data.user_id}</td>
+          <td>{this.props.data.user_name}</td>
+          <td>{this.props.data.user_url}</td>
+        </tr>
       )
   }
 }
